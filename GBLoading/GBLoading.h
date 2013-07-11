00ea5f9c32eb4cc75423733720e591baa7a9f3b0
Luka Mirosevic <luka@goonbee.com>
2013-07-11 15:00:45 +0200

    Initial Commit

---
+++ GBLoading/GBLoading.h
@@ -0,0 +1,13 @@
+//
+//  GBLoading.h
+//  GBLoading
+//
+//  Created by Luka Mirosevic on 11/07/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBLoading : NSObject
+
+@end
---
+++ GBLoading/GBLoading.m
@@ -0,0 +1,13 @@
+//
+//  GBLoading.m
+//  GBLoading
+//
+//  Created by Luka Mirosevic on 11/07/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBLoading.h"
+
+@implementation GBLoading
+
+@end
